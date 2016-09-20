51f4dd60e7acccdf857ec6ecdbea953bbd1a77f1
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-20 16:50:13 +0200

    Add Chip first approach

---
+++ src/app/Chip/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Chip, Avatar } from 'react-toolbox'
+import style from './style'
+
+export default () => (
+  <header className={style.chip}>
+    <Chip deletable>
+      <Avatar style={{backgroundColor: 'deepskyblue'}} title='P' /><span>provider:totalwind</span>
+    </Chip>
+    <Chip deletable>
+      <Avatar style={{backgroundColor: 'brown'}} title='T' /><span>type:particular</span>
+    </Chip>
+    <Chip deletable>
+      <Avatar style={{backgroundColor: 'blueviolet'}} title='S' /><span>size&lt;7</span>
+    </Chip>
+    <Chip deletable>
+      <Avatar style={{backgroundColor: 'blueviolet'}} title='S' /><span>size&gt;6</span>
+    </Chip>
+  </header>
+)
--- src/app/Panel/index.js
+++ src/app/Panel/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import style from './style'
+import Chip from '../Chip'
 import { Panel, Checkbox } from 'react-toolbox'
 
 export default () => (
   <Panel className={style.panel}>
+    <Chip />
     <h1>Main Content 0</h1>
     <p>
       Main content goes here.
