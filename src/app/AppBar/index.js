52639492dc997116b47e11d01bc7ac9d6dff1be8
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-09-22 23:23:57 +0200

    WIP

--- src/app/AppBar/index.js
+++ src/app/AppBar/index.js
@@ -4,11 +4,10 @@ import { AppBar, IconButton } from 'react-toolbox'
 
 import InputSearch from '../InputSearch'
 import theme from './theme'
-import style from './style'
 
 export default ({toggle, get, set}) => {
   return (
-    <AppBar style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }} theme={theme} fixed>
+    <AppBar theme={theme} fixed>
       <IconButton icon='menu' inverse onClick={toggle('drawerActive')} />
       <InputSearch toggle={toggle} get={get} set={set} />
       <IconButton icon='filter_list' inverse />
--- src/app/List/index.js
+++ src/app/List/index.js
@@ -9,10 +9,6 @@ export default ({toggle, get, set}) => (
   <List selectable ripple>
     <ListDivider />
     <ListSubHeader className={style.ListSubHeader} caption='150 results on 542ms' />
-    <ListItem
-      avatar='https://dl.dropboxusercontent.com/u/2247264/assets/m.jpg'
-      caption='Vendo Vela North Sails Warp 2009'
-      legend='totalwind.net'
-      rightActions={[ <Span/> ]} />
+    <ListItem avatar='https://dl.dropboxusercontent.com/u/2247264/assets/m.jpg' caption='Vendo Vela North Sails Warp 2009' legend='totalwind.net' />
   </List>
 )
